ea25634174a433523aba74c10f8d28ac98bb2051
Mohan <kmohan17official@gmail.com>
2025-04-15 13:48:32 +0530

    Task-1/DEECISION MAKING STATEMENTS/LOOPS

---
+++ Task's/Task_no_1/src/Task_no_1/Task_no_1.java
@@ -0,0 +1,112 @@
+package Task_no_1;
+import java.util.Scanner;
+
+public class Task_no_1 {
+	public static void main (String args[]) {
+
+	     	//Question-1
+	        
+	        // for(int i=10;i<=50;i++){
+	        //     System.out.println(i);
+	        // }
+	        
+	        //Question-2
+	        
+
+//	        Scanner scanner = new Scanner(System.in);
+//
+//	        int number = scanner.nextInt();
+//
+//	 
+//	        if (number > 0) {
+//	            System.out.println("Positive.");
+//	        } else if (number < 0) {
+//	            System.out.println("Negative.");
+//	        } 
+
+	        // scanner.close();
+		
+			
+			//Question-3
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int number = scanner.nextInt();
+//        
+//        String numberToString = String.valueOf(number);
+//        String reversedString = "";
+//        
+//        for (int i=numberToString.length()-1;i>=0;i--) {
+//        	reversedString += numberToString.charAt(i);
+//        }
+//        System.out.println(reversedString);
+		
+		//Question-4
+		
+//		 Scanner scanner = new Scanner(System.in);
+//
+//	        System.out.print("Enter first number: ");
+//	        int num1 = scanner.nextInt();
+//
+//	        System.out.print("Enter second number: ");
+//	        int num2 = scanner.nextInt();
+//
+//	        System.out.print("Enter third number: ");
+//	        int num3 = scanner.nextInt();
+//
+//	        int smallest;
+//
+//	        if (num1 <= num2 && num1 <= num3) {
+//	            smallest = num1;
+//	        } else if (num2 <= num1 && num2 <= num3) {
+//	            smallest = num2;
+//	        } else {
+//	            smallest = num3;
+//	        }
+//
+//	        System.out.println("The smallest number is: " + smallest);
+//	        scanner.close();
+	        
+//	      //Question-5
+//		
+//		  //5-1
+//		
+//		 Scanner scanner = new Scanner(System.in);
+//
+//	        System.out.print("Enter Amount: ");
+//	        int amt = scanner.nextInt();
+//
+//	        if(amt<500){
+//	        	System.out.println("No Discount Available and Final Amount is : "+ String.valueOf(amt));
+//	        } 
+//	        
+//	        //5-2
+//	        else if (amt>=500 && amt<=1000)
+//	        {
+//	        	System.out.println("Final Amount is : "+String.valueOf(amt-(amt*1/10)));
+//	        }
+//	        
+//	        //5-3
+//	        else {
+//	        	System.out.println("Final Amount is : "+String.valueOf(amt-(amt*1/5)));
+//	        	
+//	        }
+		
+		//Question-6
+		
+//		int n = 5;
+//
+//        for (int i = 0; i < n; i++) { 
+//            for (int j = 0; j < n; j++) { 
+//                if (j < i) {
+//                    System.out.print(n - j); 
+//                } else {
+//                    System.out.print(n - i); 
+//                }
+//            }
+//            System.out.println(); 
+//        }
+	        
+		
+	    }
+}
